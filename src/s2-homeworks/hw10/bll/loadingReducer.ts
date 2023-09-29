11bfbe1babcec1b4eadac45fbe8467a72495c16c
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-09-29 20:24:13 +0300

    Home Work 11

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,9 +2,12 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): InitStateType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
+        case 'CHANGE_LOADING': {
+            return {...state, isLoading: action.isLoading}
+        }
 
         default:
             return state
@@ -20,3 +23,7 @@ export const loadingAC = (isLoading: boolean): LoadingActionType => ({
     type: 'CHANGE_LOADING',
     isLoading,
 })
+
+export type InitStateType = {
+    isLoading: boolean
+}
