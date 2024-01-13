671da09e92b07778ecab374860239eff77077571
RuslanZhavoronkov <ruslanbear@mail.ru>
2024-01-13 23:36:28 +0300

    HW15 - 2chanse

--- src/s2-homeworks/hw15/common/c9-SuperPagination/SuperPagination.tsx
+++ src/s2-homeworks/hw15/common/c9-SuperPagination/SuperPagination.tsx
@@ -1,70 +1,74 @@
-import React, { ChangeEvent } from 'react'
-import SuperSelect from '../../../hw07/common/c5-SuperSelect/SuperSelect'
-import {Pagination} from '@mui/material'
-import s from './SuperPagination.module.css'
-import { ParamsType } from '../../HW15'
-
+import React, {ChangeEvent} from "react";
+import SuperSelect from "../../../hw07/common/c5-SuperSelect/SuperSelect";
+import {Pagination} from "@mui/material";
+import s from "./SuperPagination.module.css";
+import {ParamsType} from "../../HW15";
 
 export type SuperPaginationPropsType = {
-    id?: string
-    page: number
-    itemsCountForPage: number
-    totalCount: number
-    onChange: (page: number, count: number) => void
+  id?: string;
+  page: number;
+  itemsCountForPage: number;
+  totalCount: number;
+  onChange: (page: number, count: any) => void;
+};
+
+const SuperPagination: React.FC<SuperPaginationPropsType> = ({
+  page,
+  itemsCountForPage,
+  totalCount,
+  onChange,
+  id = "hw15",
+}) => {
+  //const lastPage = 10 // пишет студент // вычислить количество страниц
+  const lastPage = Math.ceil(totalCount / itemsCountForPage);
+
+//   const onChangeCallback = (event: ParamsType, page: number) => {
+//     // пишет студент
+//     onChange(page, event.count);
+//   };
+
+const onChangeCallback = (event:ChangeEvent<unknown>, page:number) => {
+
 }
 
-const SuperPagination: React.FC<SuperPaginationPropsType> = (
-    {
-        page, itemsCountForPage, totalCount, onChange, id = 'hw15',
-    }
-) => {
-    const lastPage = 10 // пишет студент // вычислить количество страниц
+  const onChangeSelect = (event: ChangeEvent<HTMLSelectElement>) => {
+    // пишет студент
+    const count = parseInt(event.target.value);
+    onChange(page, count);
+  };
 
-    const onChangeCallback = (event: ParamsType, page: number) => {
-        // пишет студент
+  return (
+    <div className={s.pagination}>
+      <Pagination
+        id={id + "-pagination"}
+        sx={
+          {
+            // стили для Pagination // пишет студент
+          }
+        }
+        page={page}
+        count={lastPage}
+        onChange={onChangeCallback}
+        hideNextButton
+        hidePrevButton
+      />
 
-        console.log(`You ${event}`)
-       onChange(page, event.count)
-    }
+      <span className={s.text1}>показать</span>
 
-    const onChangeSelect = (event: ChangeEvent<HTMLSelectElement>) => {
-        // пишет студент
-    }
+      <SuperSelect
+        id={id + "-pagination-select"}
+        value={itemsCountForPage}
+        options={[
+          {id: 4, value: 4},
+          {id: 7, value: 7},
+          {id: 10, value: 10},
+        ]}
+        onChange={onChangeSelect}
+      />
 
-    return (
-        <div className={s.pagination}>
-            <Pagination
-                id={id + '-pagination'}
-                sx={{
-                    // стили для Pagination // пишет студент
-                }}
-                page={page}
-                count={lastPage}
-                onChange={onChangeCallback}
-                hideNextButton
-                hidePrevButton
-            />
+      <span className={s.text2}>строк в таблице</span>
+    </div>
+  );
+};
 
-            <span className={s.text1}>
-                показать
-            </span>
-
-            <SuperSelect
-                id={id + '-pagination-select'}
-                value={itemsCountForPage}
-                options={[
-                    {id: 4, value: 4},
-                    {id: 7, value: 7},
-                    {id: 10, value: 10},
-                ]}
-                onChange={onChangeSelect}
-            />
-
-            <span className={s.text2}>
-                строк в таблице
-            </span>
-        </div>
-    )
-}
-
-export default SuperPagination
+export default SuperPagination;
